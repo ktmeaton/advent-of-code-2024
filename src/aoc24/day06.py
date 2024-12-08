ee4d55405e81bf8e5bdf31f4a8e7817e0c029174
Katherine Eaton <ktmeaton@gmail.com>
2024-12-08 00:42:40 -0700

    day 06: speedup, still couple seconds

--- src/aoc24/day06.py
+++ src/aoc24/day06.py
@@ -36,6 +36,7 @@ def day06():
     with open("data/day06.txt") as infile:
         lines = [line.strip() for line in infile]
 
+    # Locate the guard's starting position
     w, h = len(lines[0]), len(lines)
     for y,line in enumerate(lines):
         for x,c in enumerate(line):
@@ -43,17 +44,16 @@ def day06():
                 gx, gy, gd = x, y, "up"
                 break
 
+    # Find how many unique coordinates the guard enters
+    # keeping track of the direction when they first enter
     unique_coords = {(gx, gy): gd}
-    x1, y1, d = gx, gy, gd
+    x1, y1, d, x2, y2 = gx, gy, gd, gx, gy
 
-    while True:
+    while x2 >= 0 and x2 < (w-1) and y2 >= 0 and y2 < (h-1):
         x2, y2 = step_forward(x1, y1, d)
-        if x2 < 0 or x2 >= w or y2 < 0 or y2 >= h: break
-        c = lines[y2][x2]
-        while c == "#":
+        while lines[y2][x2] == "#":
             d = change_direction(d)
             x2, y2 = step_forward(x1, y1, d)
-            c = lines[y2][x2]
         x1, y1 = x2, y2
         if (x1,y1) not in unique_coords:
             unique_coords[(x1,y1)] = d
@@ -61,31 +61,15 @@ def day06():
     part1 = len(unique_coords)    
     print(f"Day 06 | Part 1: {part1}      | {'Correct' if part1 == 5531 else 'Wrong'}")
 
-    # tmp_lines = [[c for c in line] for line in lines]
-    # for x,y in unique_coords:
-    #     tmp_lines[y][x] = "X"
-    # for line in tmp_lines:
-    #     print("".join(line))
-    # Loops!
-    # We can only place one obstacle, so we limit it to coordinates that the guard traverses
-    # Which is still 5000+ coords, so brute forcing isn't my #1 strategy
-    # Or could we?
     part2  = 0
 
-    testing = [(3,6), (6,7), (7, 7)]
-    wrong = [ (8,5)]
     lines = [[c for c in line] for line in lines]
     for obxy_i,obxy in enumerate(unique_coords):
-        print(obxy_i)
         # Skip the guard's starting position
         if obxy == (gx, gy): continue
         obx, oby, d = obxy[0], obxy[1], unique_coords[obxy]
         lines[oby][obx] = "#"
-        debug_lines = copy.deepcopy(lines)
-        # # For debugging
-        # if obxy != testing[2]:
-        #     lines[oby][obx] = "."
-        #     continue
+
         # Figure out where the guard was before hiting our new obstacle
         x,y = step_backward(obx, oby, d)
         d = change_direction(d)
@@ -97,16 +81,13 @@ def day06():
             continue
 
         # If it does, this is a loop candidate
-        loop, seen = False, [[(x,y)]]
+        seen = [[(x,y)]]
         while "#" in path:
             # Tidy up latest coords
             i = [i for i,c in enumerate(path) if c == "#"][0] - 1
             coords = coords[0:i+1]
-            #print(f"x: {x}, y: {y}, d: {d}, path: {path}, coords: {coords}", coords in seen)
-
             if coords in seen:
-                loop = True
-                #print("LOOP FOUND")
+                part2 += 1
                 break
             elif coords != []:
                 seen.append(coords)
@@ -117,45 +98,11 @@ def day06():
             d = change_direction(d)
             path, coords = path_coords(x,y,d,lines)
 
-            # # Debug
-            # debug_seen = copy.deepcopy(seen)
-            # if "#" not in path:
-            #     debug_seen += [coords]
-            # for d_i,d_cxy in enumerate(debug_seen):
-            #     for d_cx,d_cy in d_cxy:
-            #         debug_lines[d_cy][d_cx] = f"{d_i}"
-            # debug_lines[gy][gx] = "^"
-            # debug_lines[oby][obx] = "O"
-            # print("-" * 20)
-            # for line in debug_lines:
-            #     print("".join(line))
-            # print("-" * 20)
-
-        #print("WHILE END")
-
-        if loop:
-            part2 += 1
-            # # Debug
-            # for i,coords in enumerate(seen):
-            #     for cx,cy in coords:
-            #         debug_lines[cy][cx] = f"{i}"
-            # debug_lines[gy][gx] = "^"
-            # debug_lines[oby][obx] = "O"
-            # print("-" * 20)
-            # for line in debug_lines:
-            #     print("".join(line))
-            # print("-" * 20)
-            # break
-
         # Reset the obstacle
         lines[oby][obx] = "."
-        debug_lines[oby][obx] = "."
 
 
-    print(f"       | Part 2: {part2}      | {'Correct' if part1 == 5531 else 'Wrong'}")
-
-    #for line in lines:
-    #    print("".join(line))
+    print(f"       | Part 2: {part2}      | {'Correct' if part2 == 2165 else 'Wrong'}")
 
 if __name__ == "__main__":
     day06()
