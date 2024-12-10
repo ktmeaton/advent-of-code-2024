beae71272fc7cf82cb92226a209c363acdd7106b
Katherine Eaton <ktmeaton@gmail.com>
2024-12-09 22:11:49 -0700

    day 08: part2 complete

--- src/aoc24/day08.py
+++ src/aoc24/day08.py
@@ -12,9 +12,9 @@ async def day08():
     # Locate all antennas
     # Calculate pairwise distances of each set and place their antinodes
 
-    part1 = 0
+    part1 = set()
+    part2 = set()
     antennas = OrderedDict()
-    coords = OrderedDict()
     for y1,line in enumerate(lines):
         for x1,c in enumerate(line):
             if c == ".": continue
@@ -22,28 +22,36 @@ async def day08():
                 antennas[c] = [(x1, y1)]
                 continue
             antennas[c].append((x1,y1))
-
             for x2,y2 in antennas[c]:
                 if x1 == x2 and y1 == y2: continue
-                a1 = x1 + (x1 - x2), y1 + (y1 - y2)
-                a2 = x2 - (x1 - x2), y2 - (y1 - y2)
-                #print(f"{c}: ({x1}, {y1}) --> ({x2}, {y2}), a1={a1}, a2={a2}")
-
-                for ax,ay in [a1, a2]:
-                    if ax >= 0 and ax < w and ay >= 0 and ay < h:
-                        coords[(ax, ay)] = c
+                for name in ["a1", "a2"]:
+                    vx, vy = (x1 - x2, y1 - y2) if name == "a1" else (x2 - x1, y2 - y1)
+                    ax, ay = (x1 + vx, y1 + vy) if name == "a1" else (x2 + vx, y2 + vy)
+                    seen_part1 = False
+                    #print("-" * 80)
+                    #print(f"x: {ax}, y: {ay}, vx: {vx}, vy: {vy}")
+                    while ax >= 0 and ax < w and ay >= 0 and ay < h:
                         debug_lines[ay][ax] = "#"
-                        #print("\t", a1)
+                        if not seen_part1:
+                            part1.add((ax, ay))
+                            seen_part1 = True
+                        part2.add((ax, ay))
+                        ax, ay = (ax + vx, ay + vy)
 
-    for line in lines:
-        print("".join(line))
-    print("-"* 80)
-    for line in debug_lines:
-        print("".join(line))
+    # for line in lines:
+    #     print("".join(line))
+    # print("-"* 80)
+    # for line in debug_lines:
+    #     print("".join(line))
 
-    part1 = len(coords)
-
+    part1 = len(part1)
     print(f"Day 08 | Part 1: {part1}      | {'Correct' if part1 == 1 else 'Wrong'}")
-    part2 = 0
+    for coords in antennas.values():
+        if len(coords) <= 1: continue
+        new = set(c for c in coords if c not in part2)
+        part2.update(new)
+        #new = [coord for coord in coords if coord not in part2]
+        #part2 += len(new)
+    part2 = len(part2)
     print(f"       | Part 2: {part2}      | {'Correct' if part2 == 1 else 'Wrong'}")
        
