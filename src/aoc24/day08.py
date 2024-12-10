28cffc0a555597be362bb99acda5da825df656cb
Katherine Eaton <ktmeaton@gmail.com>
2024-12-09 22:15:04 -0700

    day 08: golf

--- src/aoc24/day08.py
+++ src/aoc24/day08.py
@@ -1,20 +1,13 @@
-from collections import OrderedDict
-import copy
-
 async def day08():
 
     with open("data/day08.txt") as infile:
         lines = [[c for c in l.strip()] for l in infile]
+
     w,h = len(lines[0]), len(lines)
-
-    debug_lines = copy.deepcopy(lines)
-
-    # Locate all antennas
-    # Calculate pairwise distances of each set and place their antinodes
-
     part1 = set()
     part2 = set()
-    antennas = OrderedDict()
+    antennas = {}
+
     for y1,line in enumerate(lines):
         for x1,c in enumerate(line):
             if c == ".": continue
@@ -28,30 +21,18 @@ async def day08():
                     vx, vy = (x1 - x2, y1 - y2) if name == "a1" else (x2 - x1, y2 - y1)
                     ax, ay = (x1 + vx, y1 + vy) if name == "a1" else (x2 + vx, y2 + vy)
                     seen_part1 = False
-                    #print("-" * 80)
-                    #print(f"x: {ax}, y: {ay}, vx: {vx}, vy: {vy}")
                     while ax >= 0 and ax < w and ay >= 0 and ay < h:
-                        debug_lines[ay][ax] = "#"
                         if not seen_part1:
                             part1.add((ax, ay))
                             seen_part1 = True
                         part2.add((ax, ay))
                         ax, ay = (ax + vx, ay + vy)
 
-    # for line in lines:
-    #     print("".join(line))
-    # print("-"* 80)
-    # for line in debug_lines:
-    #     print("".join(line))
-
     part1 = len(part1)
-    print(f"Day 08 | Part 1: {part1}      | {'Correct' if part1 == 1 else 'Wrong'}")
-    for coords in antennas.values():
-        if len(coords) <= 1: continue
-        new = set(c for c in coords if c not in part2)
-        part2.update(new)
-        #new = [coord for coord in coords if coord not in part2]
-        #part2 += len(new)
+    print(f"Day 08 | Part 1: {part1}      | {'Correct' if part1 == 320 else 'Wrong'}")
+
+    for coords in [c for c in antennas.values() if len(c) > 1]:
+        part2.update(set(c for c in coords if c not in part2))
     part2 = len(part2)
-    print(f"       | Part 2: {part2}      | {'Correct' if part2 == 1 else 'Wrong'}")
+    print(f"       | Part 2: {part2}     | {'Correct' if part2 == 1157 else 'Wrong'}")
        
